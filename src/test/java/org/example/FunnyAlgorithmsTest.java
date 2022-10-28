871e8261ce3ea309dd071b6bf333c4a80c585679
Simone Rotundo <simone1377.rotundo@gmail.com>
2022-10-28 17:21:15 +0200

    shouldThrowExceptionIfRealNumberIsPassed test method added.

--- src/test/java/org/example/FunnyAlgorithmsTest.java
+++ src/test/java/org/example/FunnyAlgorithmsTest.java
@@ -86,6 +86,12 @@ public class FunnyAlgorithmsTest {
         });
     }
 
-
+    @Test
+    public void shouldThrowExceptionIfRealNumberIsPassed() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            String s = "123.45";
+            funnyAlgorithms.stringToIntConverter(s);
+        });
+    }
 
 }
